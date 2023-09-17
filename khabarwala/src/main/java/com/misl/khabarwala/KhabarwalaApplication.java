234d61d9c0d1eed69dd307d2875d92f44a8cfdd6
mihirbd <mstarequlislam@gmail.com>
2023-09-17 09:31:24 +0600

    project initiated

---
+++ khabarwala/src/main/java/com/misl/khabarwala/KhabarwalaApplication.java
@@ -0,0 +1,13 @@
+package com.misl.khabarwala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhabarwalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhabarwalaApplication.class, args);
+	}
+
+}
---
+++ khabarwala/src/test/java/com/misl/khabarwala/KhabarwalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misl.khabarwala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhabarwalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
